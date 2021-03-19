3fb3e697f53679c44e7152c253f23d97574916eb
Nhatdealin <dominhnhat2311@gmail.com>
2021-03-20 00:37:11 +0100

    add files, update code, finish project

--- data_generator.py
+++ data_generator.py
@@ -2,7 +2,7 @@ import os
 import Augmentor
 
 
-TRAIN_PATH = "./processed_data/train/"
+TRAIN_PATH = "./Dataset_RGB_corr_3_folder/train_10/"
 
 
 def generate_data(output_file, path):
@@ -13,17 +13,17 @@ def generate_data(output_file, path):
     # and augmented in parallel to the original data.
     # p.ground_truth("/path/to/ground_truth_images")
     # Add operations to the pipeline as normal:
-    p.rotate(probability=1, max_left_rotation=14, max_right_rotation=14)
+    p.rotate(probability=0.7, max_left_rotation=14, max_right_rotation=14)
     # p.flip_left_right(probability=0.5)
-    p.zoom_random(probability=0.5, percentage_area=0.8)
+    # p.zoom_random(probability=0.5, percentage_area=0.8)
     # p.flip_top_bottom(probability=0.5)
-    p.random_distortion(probability=0.7, grid_width=5, grid_height=5, magnitude=8)
-    p.flip_left_right(probability=0.3)
+    p.random_distortion(probability=0.2, grid_width=2, grid_height=2, magnitude=8)
+    p.flip_left_right(probability=0.4)
     p.flip_top_bottom(probability=0.4)
-    p.rotate_random_90(probability=0.3)
-    p.skew(0.2, 0.7)
-    p.skew_tilt(0.6, 0.7)
-    p.sample(100)
+    # p.rotate_random_90(probability=0.3)
+    p.skew(0.1, 0.2)
+    p.skew_tilt(0.2, 0.7)
+    p.sample(400)
 
 
 if __name__ == '__main__':
